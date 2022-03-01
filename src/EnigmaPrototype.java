0eac94ce49d5e40384688c842600169364fcb95b
benj302g <benj302g@stud.kea.dk>
2022-03-01 11:27:05 +0100

    first commit

---
+++ src/EnigmaPrototype.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class EnigmaPrototype {
+
+    Scanner input = new Scanner(System.in);
+    String alfabet = " abcdefghijklmnopqrstuvxyzæøå";
+
+    public int caesarEncryption(char bogstav) {
+
+        int indexOfChar = 0;
+        indexOfChar = alfabet.indexOf(bogstav);
+
+        return indexOfChar;
+    }
+
+    public char caesarDecrypting(int number) {
+        char charAtIndex = alfabet.charAt(number);
+
+        return charAtIndex;
+    }
+
+    public static void main(String[] args) {
+        new EnigmaPrototype().caesarEncryption('g');
+        System.out.println(new EnigmaPrototype().caesarEncryption('g'));
+        System.out.println(new EnigmaPrototype().caesarDecrypting(7));
+    }
+}
